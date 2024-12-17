0511cbf7306c33445f06bbd8ee74c76671cb38d1
5dt1 <contact@5dt1.com>
2024-12-17 21:52:52 +0100

    Fixed and refactored ScathaMining

--- src/main/java/com/cinqdt1/Mod/features/ScathaMining.java
+++ src/main/java/com/cinqdt1/Mod/features/ScathaMining.java
@@ -9,12 +9,14 @@ import com.cinqdt1.Mod.utils.RenderUtils;
 import com.cinqdt1.Mod.utils.Utils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.StringUtils;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,42 +48,35 @@ public class ScathaMining {
     }
 
     @SubscribeEvent
-    public void onTick(TickEvent.ClientTickEvent event) {
+    public void onTick(@NotNull TickEvent.ClientTickEvent event) {
         if (event.phase != TickEvent.Phase.START) return;
         if (!isScathaSpawned) return;
+        if (scathaSpawnTime - System.currentTimeMillis() >= 5000)
+        {
+            isScathaSpawned = false;
+            return;
+        }
 
         Minecraft mc = Minecraft.getMinecraft();
         List<Entity> listWorldEntity = mc.theWorld.getLoadedEntityList();
         for (Entity entity : listWorldEntity) {
-            switch (scathaType) {
-                case UNKNOWN:
-                    if (entity.getName().contains("Scatha")) {
-                        scathaType = SCATHA_TYPE.SCATHA;
-                        Utils.sendModMessage("Scatha detected");
-                        Utils.showTitle("Scatha Spawned", "");
-                    } else if (entity.getName().contains("Worm")) {
-                        scathaType = SCATHA_TYPE.WORM;
-                        Utils.sendModMessage("Worm detected");
-                        //if(ModConfiguration.scathaAlert)
-                        Utils.showTitle("Worm Spawned", "");
-                    }
-                    break;
-                case SCATHA:
-                    if (entity.getName().contains("Scatha")) {
-                        break;
-                    }
-                    isScathaSpawned = false;
-                    scathaType = SCATHA_TYPE.UNKNOWN;
-                    break;
-                case WORM:
-                    if (entity.getName().contains("Worm")) {
-                        break;
-                    }
-                    isScathaSpawned = false;
-                    scathaType = SCATHA_TYPE.UNKNOWN;
-                    break;
+            BlockPos entityPos = entity.getPosition();
+            double distance = mc.thePlayer.getPosition().distanceSq(entityPos);
+            if (distance > 200)
+                continue;
+            if (entity.getName().contains("Scatha")) {
+                scathaType = SCATHA_TYPE.SCATHA;
+                Utils.sendModMessage("Scatha detected");
+                Utils.showTitle("Scatha Spawned", "");
+                isScathaSpawned = false;
+                break;
+            } else if (entity.getName().contains("Worm")){
+                scathaType = SCATHA_TYPE.WORM;
+                Utils.sendModMessage("Worm detected");
+                Utils.showTitle("Worm Spawned", "");
+                isScathaSpawned = false;
+                break;
             }
-
         }
     }
 
